b686bf0078cea8d075c26acaa4e1e97eb5a86e01
Mara Regina <maramrps@gmail.com>
2011-12-05 16:32:03 -0200

    correção na classe filtroweb

--- src/java/filtros/FiltroWeb.java
+++ src/java/filtros/FiltroWeb.java
@@ -1,9 +1,5 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package filtros;
 
+package filtros;
 import java.io.IOException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -15,25 +11,24 @@ import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-/**
- *
- * @author Mara
- */
+
 @WebFilter(filterName = "FiltroWeb", urlPatterns = {"/protegido/*"})
 public class FiltroWeb implements Filter {
-    
+    public void init(FilterConfig filterConfig) throws ServletException {
+        System.out.println("Iniciou");
+    }
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         try{
 
             HttpSession session = ((HttpServletRequest) request).getSession(false);
             if (session != null) {
                 if((session.getAttribute("LOGADO")!=null)&&(session.getAttribute("LOGADO").equals("SIM"))){
-                    
+                    System.out.println("Usuario Logado");
                     chain.doFilter(request, response);
                 }
                 else{
-                    
-                    ((HttpServletResponse)response).sendRedirect("error.xhtml");
+                    System.err.println("Usuario NÃO Logado");
+                    ((HttpServletResponse)response).sendRedirect("../error.xhtml");
                 }
             }
             
@@ -41,16 +36,8 @@ public class FiltroWeb implements Filter {
             e.printStackTrace();
         }
     }
-
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
     public void destroy() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        System.out.println("Finalizou");
     }
-    
 
 }
